eb9adf79c4af682b5a3861dd76301f53c7a0d830
Liam Von Astoria <liam.dispa@skygenesisenterprise.com>
2025-10-26 11:41:01 +0100

    Update Docker environment & Backend API

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{Filter, Reply};
+use std::convert::Infallible;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello")
+        .map(|| "Hello, World!");
+
+    println!("Server started at http://localhost:3000");
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3000))
+        .await;
 }
