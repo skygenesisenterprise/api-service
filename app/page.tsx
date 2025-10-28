679f2f67c7192bae67658e47de5e068c50f5000f
Liam Von Astoria <liam.dispa@skygenesisenterprise.com>
2025-10-28 16:12:59 +0100

    Update package.json environment

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,13 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useAuthContext } from "@/app/context/AuthContext";
 
 export default function Home() {
-  const { isAuthenticated } = useAuthContext();
   const router = useRouter();
 
   useEffect(() => {
-    if (isAuthenticated) router.replace("/pages/admin");
-    else router.replace("/pages/auth/login");
-  }, [isAuthenticated, router]);
+    router.replace("/login");
+  }, [router]);
 
   return null;
 }
